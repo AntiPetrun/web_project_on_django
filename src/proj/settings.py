cd9131b30736c36f1fffe51aaad38e937ab95839
AntiPetrun <104591875+AntiPetrun@users.noreply.github.com>
2022-07-05 20:17:07 +0300

    added static and media url and roots in settings

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Minsk'
 
 USE_I18N = True
 
@@ -116,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/home/AntiPetrun86/web_project_on_django/static'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = '/home/AntiPetrun86/web_project_on_django/media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
