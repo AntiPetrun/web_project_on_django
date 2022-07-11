6e0ae5837b661931a130a1babdc73c651b233e0c
AntiPetrun <104591875+AntiPetrun@users.noreply.github.com>
2022-07-11 21:24:34 +0300

    some imrovements

--- src/reference_books/models.py
+++ src/reference_books/models.py
@@ -1,5 +1,6 @@
 from email.mime import image
 from pyexpat import model
+from unicodedata import name
 from django.db import models
 from django.forms import DateField
 
@@ -16,13 +17,15 @@ class Author(models.Model):
         verbose_name= 'Last Name',
         max_length= 32
     )
-    short_name = models.CharField(
-        verbose_name= 'Short Name',
+    full_name = models.CharField(
+        verbose_name= 'Full Name',
         max_length= 32
     )
     country_birth = models.CharField(
         verbose_name= 'Country of Birth',
-        max_length= 32
+        max_length= 32,
+        blank= True,
+        null= True
     )
     date_birth = models.DateField(
         verbose_name= 'Date of Birth',
@@ -31,12 +34,14 @@ class Author(models.Model):
     )
     biography = models.TextField(
         verbose_name= 'Biography',
-        max_length= 256
+        max_length= 256,
+        blank= True,
+        null= True
     )
     
     
     def __str__(self) -> str:
-        return self.short_name
+        return self.full_name
 
 
 class BookSeries(models.Model):
@@ -53,33 +58,35 @@ class BookSeries(models.Model):
         blank= True,
         null= True
     )
-    author = models.CharField(
-        verbose_name= 'Author',
-        max_length= 32
-    )
     genre = models.CharField(
         verbose_name= 'Genre',
         max_length= 32
     )
     country = models.CharField(
         verbose_name= 'Country',
-        max_length= 32
-    )
-    language = models.CharField(
-        verbose_name= 'Language',
-        max_length= 32
-    )
-    publish_house = models.CharField(
-        verbose_name= 'Publishing House',
-        max_length= 32
-    )
-    publish_date = models.ImageField(
-        verbose_name= 'Publication Date',
+        max_length= 32,
         blank= True,
         null= True
     )
-    rus_publish_date = models.ImageField(
-        verbose_name= 'Publication Date on Russian',
+    language = models.CharField(
+        verbose_name= 'Language',
+        max_length= 32,
+        blank= True,
+        null= True
+    )
+    publish_house = models.CharField(
+        verbose_name= 'Publishing House',
+        max_length= 32,
+        blank= True,
+        null= True
+    )
+    release_date = models.DateField(
+        verbose_name= 'Release Date',
+        blank= True,
+        null= True
+    )
+    rus_release_date = models.DateField(
+        verbose_name= 'Release Date in Russian',
         blank= True,
         null= True
     )
@@ -96,7 +103,9 @@ class LiteraryGenre(models.Model):
     )
     description = models.TextField(
         verbose_name= 'Biography',
-        max_length= 256
+        max_length= 256,
+        blank= True,
+        null= True
     )
     
     
@@ -111,21 +120,47 @@ class PublishingHouse(models.Model):
     )
     country = models.CharField(
         verbose_name= 'Country',
-        max_length= 32
+        max_length= 32,
+        blank= True,
+        null= True
     )
-    founding_date = models.ImageField(
+    founding_date = models.DateField(
         verbose_name= 'Founding Date',
         blank= True,
         null= True
     )
     address = models.TextField(
         verbose_name= 'Address',
-        max_length= 256
+        max_length= 256,
+        blank= True,
+        null= True
     )
     web_site = models.URLField(
         verbose_name= 'Web Site',
         blank= True,
         null= True
+    )
+    
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(
+        verbose_name= 'Name',
+        max_length= 64
+    )
+    
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(
+        verbose_name= 'Language',
+        max_length= 32
     )
     
     
