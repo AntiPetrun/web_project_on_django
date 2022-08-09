8fcfc9b7331e34a435f4993cbfc955270bff5d13
Anton Petrunikov <apetrunikov@tutanota.com>
2022-08-09 14:05:14 +0300

    added static

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from . import local_var
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zm0_8il+1sj!tm!z17d49(*4yk#0za!@x8ol-l8_lp*d$8spqu'
+SECRET_KEY = local_var.DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
